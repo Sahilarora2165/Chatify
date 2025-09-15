b0a84aec753548fb130f28f9bc03f8019598598e
Sahil <asahilaroras1903@gmail.com>
2025-09-15 11:10:42 +0530

    Initial Spring Boot project setup with User and Chat Message entity and repository

---
+++ src/main/java/com/chatify/chat_backend/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.chatify.chat_backend.repository;
+
+import com.chatify.chat_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    // For LOGIN: Find a user by their email to verify password
+    Optional<User> findByEmail(String email);
+
+    // For REGISTRATION: Check if email is already taken (efficient exists check)
+    Boolean existsByEmail(String email);
+
+    // For REGISTRATION: Check if username is already taken (if username must be unique)
+    Boolean existsByUsername(String username);
+
+}
+
