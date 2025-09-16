390a230cfacff92224054e1b34ae9f99247cf549
Sahil <asahilaroras1903@gmail.com>
2025-09-16 21:11:58 +0530

    Add AuthResponseDTO

---
+++ src/main/java/com/chatify/chat_backend/dto/AuthResponseDTO.java
@@ -0,0 +1,15 @@
+package com.chatify.chat_backend.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthResponseDTO {
+
+    private String token;
+
+    private Long userId;
+
+    private String username;
+
+    private String email;
+}
