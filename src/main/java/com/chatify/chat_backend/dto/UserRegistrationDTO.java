20ee42e27a86b6caf180fd416798260307bc6886
Sahil <asahilaroras1903@gmail.com>
2025-09-16 21:11:40 +0530

    Add UserRegistrationDTO

---
+++ src/main/java/com/chatify/chat_backend/dto/UserRegistrationDTO.java
@@ -0,0 +1,13 @@
+package com.chatify.chat_backend.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRegistrationDTO {
+
+    private String username;
+
+    private String email;
+
+    private String password;
+}
